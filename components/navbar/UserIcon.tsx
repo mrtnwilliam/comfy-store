40128c3df774532eaab2a4a362fcb880ca70e57b
mrtnwilliam <mrtnwilliam41@gmail.com>
2025-01-13 16:58:24 +0200

    Added Auth Provider

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,17 @@
-function UserIcon() {
+import { currentUser } from "@clerk/nextjs/server"
+import { LuUserRound } from "react-icons/lu";
+
+async function UserIcon() {
+
+  const user = await currentUser();
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return <img src={profileImage} className="w-6 h-6 rounded-full object-cover" />
+  }
+
   return (
-    <div>UserIcon</div>
+    <LuUserRound className="w-10 h-10 bg-primary rounded-full text-white"/>
   )
 }
 
